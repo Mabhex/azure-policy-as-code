3f101330e4e5f02e86e4a6cf61d7d97e99dfcbe1
Bheki <Bheki.Mhlongo@standardbank.co.za>
2023-09-21 07:23:11 +0200

    Commit from VSCode

---
+++ .github/Scribble.ps1
@@ -0,0 +1,2 @@
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindowsx64 -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi
+                                                                 
